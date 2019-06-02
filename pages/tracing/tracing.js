aa1de2ce8a2e2265534994de3a72b22a3e51cc7e
JsonMa <mahao@iot.chinamobile.com>
2019-06-02 21:16:32 +0800

    修改溯源码页

--- pages/tracing/tracing.js
+++ pages/tracing/tracing.js
@@ -13,69 +13,72 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.showLoading()
     let type = options.type || "outer_code" //inner_code outer_code
-    let id = options.id || '0110c64a7cb7f8048e6a1071095c3926d64209dfe2e600c021616b15aa5b7a088c385a526970c3910e249d847e61f90248935ca77aa733019dccf880b3adb97ed9'
+    let id = options.id
+    console.log(options)
     this.data.id = id
     wx.request({
       url: baseUrl + '/tracings/' + id,
-      success:res=>{
-        let data = res.data.data.data
-        // 溯源袋内码
-        if (type == "inner_code"){
-          //都跳到绑定商品页，若已绑定，只显示列表，不能进行操作
+      success: res => {
+        wx.hideLoading()
+        if (res.data.code === 0) {
+          let data = res.data.data.data
+          // 溯源袋内码
+          if (type == "inner_code") {
+            //都跳到绑定商品页，若已绑定，只显示列表，不能进行操作
             wx.reLaunch({
               url: '/pages/bind/bind?id=' + id,
             })
-          
-        }
-        // 溯源码外袋
-        if (type == "outer_code") {
-          switch (data.state){
-            case "UNBIND": //未绑定商品
-                // todo 
+          }
+          // 溯源码外袋
+          if (type == "outer_code") {
+            switch (data.state) {
+              case "UNBIND":
                 // 提示未绑定商品
                 wx.hideLoading();
                 wx.showToast({
-                  title: '未绑定商品',
-                  duration: 2000,
-                  success:function(){
-                    wx.reLaunch({
-                      url: '/pages/home/home',
-                    })
+                  title: '该溯源码未绑定商品，无法进入溯源流程',
+                  duration: 3000,
+                  icon: 'none',
+                  complete: function() {
+                    setTimeout(() => {
+                      wx.reLaunch({
+                        url: '/pages/home/home',
+                      })
+                    }, 2000)
                   }
                 })
                 break;
-            case "BIND": 
-              //TODO
-              wx.reLaunch({
-                url: '/pages/send/send?id=' + id,
-              })
-              break;
-            case "SEND": //经销商已发货
-              //TODO
-              wx.reLaunch({
-                url: '/pages/check/check?id=' + id,
-              })
-              break;
-            case "EXPRESSED": //已绑定快递信息
-              //TODO
-              wx.reLaunch({
-                url: '/pages/send/send?id=' + id,
-              })
-              break;
-            case "REVEIVED": //客户已收回
-              //TODO
-              break; 
-
+              case "BIND":
+                wx.reLaunch({
+                  url: '/pages/send/send?id=' + id,
+                })
+                break;
+              case "SEND": //经销商已发货
+                wx.reLaunch({
+                  url: '/pages/check/check?id=' + id,
+                })
+                break;
+              case "EXPRESSED": //已绑定快递信息
+                wx.reLaunch({
+                  url: '/pages/send/send?id=' + id,
+                })
+                break;
+              case "REVEIVED": //客户已收货
+                wx.reLaunch({
+                  url: '/pages/check/check?id=' + id,
+                })
+                break;
+            }
           }
-          
-          if (data.steate == "UNBIND") {
-            
-          }else{
-
-          }
+        } else {
+          wx.showToast({
+            title: res.data.msg,
+            duration: 2000,
+            icon: 'none'
+          })
         }
       }
     })
@@ -84,49 +87,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
