f6de6780dfbb60c572e415829998ff0cc56df8d8
JsonMa <mahao@iot.chinamobile.com>
2019-06-16 23:37:17 +0800

    修复验货bug

--- pages/bind/bind.js
+++ pages/bind/bind.js
@@ -8,81 +8,80 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id:'',
+    id: '',
     baseUrl: baseUrl,
-    switch_title:'发货至经销商',
-    switch_checked:true,
-    showPicker:false,
+    switch_title: '发货至经销商',
+    switch_checked: true,
+    showPicker: false,
     columns: [],
-    goods:[],
-    isBind:false,
-    bind_goods:[],
-    showDialog:false
+    goods: [],
+    isBind: false,
+    bind_goods: [],
+    showDialog: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  switchOnChange:function(e){
+  switchOnChange: function(e) {
     this.setData({
-      switch_title: e.detail ? '发货至经销商' :'发货给消费者',
+      switch_title: e.detail ? '发货至经销商' : '发货给消费者',
       switch_checked: e.detail
     })
   },
-  openPicker:function(){
+  openPicker: function() {
     this.setData({
-      showPicker:true
+      showPicker: true
     })
   },
-  picked: function (event){
-    const { picker, value, index } = event.detail
+  picked: function(event) {
+    const {
+      picker,
+      value,
+      index
+    } = event.detail
     let goods_temp = this.data.goods
     goods_temp.push(value)
     this.setData({
-      showPicker:false,
-      goods:goods_temp
-      
+      showPicker: false,
+      goods: goods_temp
+
     })
   },
-  onCancel:function(){
+  onCancel: function() {
     this.setData({
       showPicker: false,
     })
   },
-  deleteGoods:function(e){
+  deleteGoods: function(e) {
     let index = e.target.dataset.index
     let goods_temp = this.data.goods
-    goods_temp.splice(index,1)
+    goods_temp.splice(index, 1)
     this.setData({
-      goods:goods_temp
+      goods: goods_temp
     })
   },
-  gohome:function(){
+  gohome: function() {
     wx.reLaunch({
       url: '/pages/home/home',
     })
   },
-  commit:function(){
-    // wx.showToast({
-    //   title: '成功',
-    //   icon: 'success',
-    //   duration: 2000
-    // })
+  commit: function() {
     let _this = this
     let goods = this.data.goods
     let products = []
-    for (let i=0;i<goods.length;i++){
+    for (let i = 0; i < goods.length; i++) {
       products[i] = goods[i].value
     }
     let commitData = {
-      operation:'bind',
-      products:products,
+      operation: 'bind',
+      products: products,
     }
     wx.request({
       url: baseUrl + '/tracings/' + _this.data.id,
-      method:'put',
-      data:commitData,
-      success:function(res){
+      method: 'put',
+      data: commitData,
+      success: function(res) {
         if (res.data.code == 0) {
           wx.showToast({
             title: '绑定成功',
@@ -101,33 +100,41 @@ Page({
             duration: 2000,
           })
         }
-        
+
       }
     })
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     let id = options.id || '01422ec7950351540937c7b9876a22c3fe990bc489626e0ead1fefd85df0b9e63cdf94360e7d0d610664387615946e79746732f0ba3de5ab0f93efa45f02dcaada'
     this.setData({
-      id : id
+      id: id
     })
     wx.request({
-      url: baseUrl +'/tracings/'+id,
-      success:res=>{
-          this.setData({
-            isBind: res.data.data.data.state =="UNBIND"?false:true,
-            bind_goods:res.data.data.data.products
+      url: baseUrl + '/tracings/' + id,
+      success: res => {
+        const isBind = res.data.data.data.state == "UNBIND" ? false : true;
+        this.setData({
+          isBind,
+          bind_goods: res.data.data.data.products
+        })
+        if (isBind) {
+          wx.showToast({
+            title: '',
+            icon: 'none',
+            duration: 2000,
           })
+        }
       }
     })
     wx.request({
-      url: baseUrl +'/barcodes',
-      success:res =>{
+      url: baseUrl + '/barcodes',
+      success: res => {
         let temp = res.data.data.data
         let goods_temp = []
-        for(let i=0;i<temp.length;i++){
+        for (let i = 0; i < temp.length; i++) {
           goods_temp[i] = {
-            text:temp[i].name,
-            value:temp[i]._id,
+            text: temp[i].name,
+            value: temp[i]._id,
           }
         }
         this.setData({
@@ -140,49 +147,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
--- pages/check/check.js
+++ pages/check/check.js
@@ -2,14 +2,13 @@
 const app = getApp();
 const baseUrl = app.globalData.HOST;
 const util = require('../../utils/util.js')
-const userInfo = wx.getStorageSync('userInfo')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    notice_text:'',
+    notice_text: '',
     baseUrl: baseUrl,
     banner: '',
     bind_goods: [],
@@ -17,30 +16,30 @@ Page({
     id: '',
     key: '',
     steps: [],
-    isOnwer:false,
-    showCommit: true,
+    isOnwer: false,
+    showCommit: false,
     isReceved: false,
-    showBackHome:true,
-    hasReseverInfo:false,
+    showBackHome: true,
+    hasReseverInfo: false,
   },
-  goToRight: function () {
+  goToRight: function() {
     wx.navigateTo({
       url: '/pages/rights/rights?key=' + this.data.id + '&id=' + this.data.key,
     })
   },
-  goHome: function () {
+  goHome: function() {
     wx.reLaunch({
       url: '/pages/home/home',
     })
   },
-  commit: function () {
+  commit: function() {
     wx.request({
       url: baseUrl + '/tracings/' + this.data.id,
       method: 'put',
       data: {
         operation: 'receive'
       },
-      success: function (res) {
+      success: function(res) {
         if (res.data.code == 0) {
           wx.showToast({
             title: '确认成功',
@@ -59,62 +58,107 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    let id = options.id || "01c825a971d647d89b05fd10d3f6090dca81ebd5125ea7167e6d9a39ceb1fd3e0a06dbc5b4e68b4c199fd48764e3a8d564abcf0e417d765f73f618e89f4042e4c2"
+  onLoad: function(options) {
+    let id = options.id || "015ceda323bfab56cdcdc640d69b7b31f164e65881d0f4291666b7d01744e9020e139ac1c08fbb152babcb966ba02b43d3bca2ec3e7d9daec220b6942183d38a01"
     this.data.id = id
     let _this = this
-    wx.request({
-      url: baseUrl + '/tracings/' + id,
+    wx.login({
       success: res => {
-        this.data.isOnwer = res.data.data.data.owner._id == wx.getStorageSync('userInfo').user_id ? true : false
-        this.setData({
-          key: res.data.data.data._id
-        })
-        let records = res.data.data.data.records;
-        let steps_temp = []
-        let banner = ''
-        for (let i = 0; i < records.length; i++) {
-          const sender = records[i].sender
-          const name = sender[sender.role_type].name
-          steps_temp[i] = {
-            text: records[i].express_name || name,
-            desc: records[i].express_no || util.convertUTCTimeToLocalTime(records[i].send_at),
-          }
-          if (i == records.length - 1) {
-            banner = sender[sender.role_type].banner
-            _this.setData({
-              hasReseverInfo: records[i].reciver_name?true:false
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.showLoading()
+        wx.request({
+          url: baseUrl + '/auth/login',
+          method: "post",
+          data: {
+            "code": res.code
+          },
+          success: function(res) {
+            let role_type = ''
+            let user_id = ''
+            const result = res.data.data.data
+            if (result.isRegistered == false) {
+              role_type = 4
+            } else {
+              role_type = result.user.role_type
+              user_id = result.user._id
+            }
+            const userInfo = {
+              role_type: role_type,
+              user_id: user_id
+            }
+            wx.setStorageSync('userInfo', userInfo)
+            wx.request({
+              url: baseUrl + '/tracings/' + id,
+              success: res => {
+                wx.hideLoading()
+                const result = res.data.data.data
+                _this.data.isOnwer = result.owner._id == userInfo.user_id ? true : false
+                _this.setData({
+                  key: result._id
+                })
+                let records = result.records;
+                const recordsLength = records.length
+                const latestRecord = records.slice(recordsLength - 1, recordsLength)[0]
+                let hasCommitRight = true // 如果收货人为商家，则验证是否为
+                if (latestRecord.reciver_type === 'business') {
+                  hasCommitRight = latestRecord.reciver === userInfo.user_id
+                }
+                let steps_temp = []
+                let banner = ''
+                for (let i = 0; i < records.length; i++) {
+                  const sender = records[i].sender
+                  const name = sender[sender.role_type].name
+                  steps_temp[i] = {
+                    text: records[i].express_name || name,
+                    desc: records[i].express_no || util.convertUTCTimeToLocalTime(records[i].send_at),
+                  }
+                  if (i == records.length - 1) {
+                    banner = sender[sender.role_type].banner
+                    _this.setData({
+                      hasReseverInfo: records[i].reciver_name ? true : false
+                    })
+                  }
+                }
+                const owner = result.owner
+                const state = result.state
+                let notice_text = ""
+                if (latestRecord.sender._id === userInfo.user_id & state === 'SEND') {
+                  wx.showToast({
+                    title: '您已发货，只能查看溯源码详情',
+                    icon: 'none',
+                    duration: 3000
+                  })
+                }
+                if (state == "BIND") {
+                  notice_text = "正品待售"
+                } else if (state == "SEND") {
+                  if (latestRecord.reciver_type == "consumer") {
+                    notice_text = "正品在" + util.getHours(latestRecord.send_at) + "小时前被出售"
+                  } else {
+                    notice_text = "正品待售"
+                  }
+                } else if (state == "RECEIVED") {
+                  if (res.data.data.data.isEnd == true) {
+                    notice_text = "正品在:" + util.getHours(latestRecord.reciver_at) + "小时前被签收"
+                  } else {
+                    notice_text = "正品待售"
+                  }
+                }
+                _this.setData({
+                  bind_goods: result.products,
+                  steps: steps_temp,
+                  banner: banner,
+                  id: id,
+                  showCommit: ["SEND", "EXPRESSED"].includes(state) && hasCommitRight ? true : false,
+                  isReceved: state == "RECEIVED" ? true : false,
+                  notice_text: notice_text,
+                })
+              },
+              fail: () => {
+                wx.hideLoading()
+              }
             })
-            console.log(_this.data.hasReseverInfo)
           }
-        }
-        const owner = res.data.data.data.owner
-        const state = res.data.data.data.state        
-        let notice_text = ""
-        if(state == "BIND"){
-          notice_text="正品待售"
-        }else if(state == "SEND"){
-          if (records[records.length - 1].reciver_type =="consumer"){
-            notice_text = "正品在" + util.getHours(records[records.length - 1].send_at) +"小时前被出售"
-          }else{
-            notice_text = "正品待售"
-          }
-        } else if (state == "RECEIVED"){
-          if (res.data.data.data.isEnd == true){
-            notice_text = "正品在:" + util.getHours(records[records.length - 1].reciver_at) + "小时前被签收"
-          } else {
-            notice_text = "正品待售"
-          }
-        }
-        this.setData({
-          bind_goods: res.data.data.data.products,
-          steps: steps_temp,
-          banner: banner,
-          id: id,
-          showCommit: res.data.data.data.owner._id == userInfo.user_id ? true : false,
-          isReceved: state == "RECEIVED" ? true : false,
-          notice_text: notice_text,
-          
         })
       }
     })
@@ -123,49 +167,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
