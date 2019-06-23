610eee1a71b1fc3173ca602363be8ca1d3a0ac7b
JsonMa <mahao@iot.chinamobile.com>
2019-06-24 00:02:51 +0800

    修复手机号重复验证BUG

--- pages/check/check.js
+++ pages/check/check.js
@@ -23,10 +23,9 @@ Page({
     hasReseverInfo: false,
     showDetail: false,
     hasRight: false,
-    showDialog: true,
-    wechatPhone: '',
-    firstGoods:{},
-    goodsTotal:0
+    showDialog: false,
+    firstGoods: {},
+    goodsTotal: 0
   },
   goToRight: function() {
     wx.navigateTo({
@@ -38,7 +37,7 @@ Page({
       url: '/pages/home/home',
     })
   },
-  goIntro:function(){
+  goIntro: function() {
     wx.navigateTo({
       url: "/pages/intro/intro",
     })
@@ -80,137 +79,8 @@ Page({
         iv
       },
       success: function(res) {
-        _this.setData({
-          wechatPhone: res.data.data.data.data.phoneNumber
-        })
-        const id = _this.data.id
-        wx.login({
-          success: res => {
-            // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            wx.showLoading()
-            wx.request({
-              url: baseUrl + '/auth/login',
-              method: "post",
-              data: {
-                "code": res.code
-              },
-              success: function (res) {
-                let role_type = ''
-                let user_id = ''
-                const result = res.data.data.data
-                if (result.isRegistered == false) {
-                  role_type = 4
-                } else {
-                  role_type = result.user.role_type
-                  user_id = result.user._id
-                }
-                const userInfo = {
-                  role_type: role_type,
-                  user_id: user_id
-                }
-                wx.setStorageSync('userInfo', userInfo)
-                wx.request({
-                  url: baseUrl + '/tracings/' + id,
-                  success: res => {
-                    wx.hideLoading()
-                    const result = res.data.data.data
-                    _this.data.isOnwer = result.owner._id == userInfo.user_id ? true : false
-                    _this.setData({
-                      key: result._id
-                    })
-                    let records = result.records;
-                    const recordsLength = records.length
-                    if (recordsLength === 0 || ['BIND', 'UNBIND'].includes(result.state)) {
-                      wx.showToast({
-                        title: '未发货，无法查看溯源码详情',
-                        icon: 'none',
-                        duration: 3000
-                      })
-                      return
-                    } else {
-                      _this.setData({
-                        showDetail: true
-                      })
-                      wx.showToast({
-                        title: '主人好，请鉴',
-                        icon: 'none',
-                        duration: 3000
-                      })
-                    }
-                    const latestRecord = records.slice(recordsLength - 1, recordsLength)[0]
-                    let hasCommitRight = true
-                    if (latestRecord.reciver_type === 'business') {
-                      hasCommitRight = latestRecord.reciver === userInfo.user_id
-                    } else {
-                      if (latestRecord.reciver_phone) {
-                        hasCommitRight = latestRecord.reciver_phone === _this.data.wechatPhone
-                      } else {
-                        hasCommitRight = latestRecord.sender !== userInfo.user_id
-                      }
-                    }
-                    let steps_temp = []
-                    let banner = ''
-                    for (let i = 0; i < records.length; i++) {
-                      const sender = records[i].sender
-                      const name = sender[sender.role_type].name
-                      steps_temp[i] = {
-                        text: records[i].express_name || name,
-                        desc: records[i].express_no || util.convertUTCTimeToLocalTime(records[i].send_at),
-                      }
-                      if (i == records.length - 1) {
-                        banner = sender[sender.role_type].banner
-                        _this.setData({
-                          hasReseverInfo: records[i].reciver_name ? true : false
-                        })
-                      }
-                    }
-                    const owner = result.owner
-                    const state = result.state
-                    let notice_text = ""
-                    if (latestRecord.sender._id === userInfo.user_id & state === 'SEND') {
-                      wx.showToast({
-                        title: '您已发货，只能查看溯源码详情',
-                        icon: 'none',
-                        duration: 3000
-                      })
-                    }
-                    if (state == "BIND") {
-                      notice_text = "正品待售"
-                    } else if (state == "SEND") {
-                      if (latestRecord.reciver_type == "consumer") {
-                        notice_text = "正品在" + util.getHours(latestRecord.send_at) + "小时前被出售"
-                      } else {
-                        notice_text = "正品待售"
-                      }
-                    } else if (state == "RECEIVED") {
-                      if (owner._id === userInfo.user_id) _this.setData({
-                        hasRight: true
-                      })
-                      if (res.data.data.data.isEnd == true) {
-                        notice_text = "正品在:" + util.getHours(latestRecord.reciver_at) + "小时前被签收"
-                      } else {
-                        notice_text = "正品待售"
-                      }
-                    }
-                    _this.setData({
-                      bind_goods: result.products,
-                      firstGoods: result.products[0],
-                      goodsTotal: result.products.length,
-                      steps: steps_temp,
-                      banner: banner,
-                      id: id,
-                      showCommit: ["SEND", "EXPRESSED"].includes(state) && hasCommitRight ? true : false,
-                      isReceved: state == "RECEIVED" ? true : false,
-                      notice_text: notice_text,
-                    })
-                  },
-                  fail: () => {
-                    wx.hideLoading()
-                  }
-                })
-              }
-            })
-          }
+        wx.reLaunch({
+          url: `/pages/check/check?id=${_this.data.id}`,
         })
       }
     })
@@ -221,6 +91,144 @@ Page({
   onLoad: function(options) {
     let id = options.id || "01de2b026a849596592e8bf2c2c7a3c6566a24f6bac386dacfa5ecfc55335c978325b8e3df19be2f0b8c4f0e72798d1df7398194958f0e4c10e73eee7a480591a8"
     this.data.id = id
+    const _this = this
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.showLoading()
+        wx.request({
+          url: baseUrl + '/auth/login',
+          method: "post",
+          data: {
+            "code": res.code
+          },
+          success: function(res) {
+            let role_type = ''
+            let user_id = ''
+            const result = res.data.data.data
+            if (result.isRegistered == false) {
+              role_type = 4
+            } else {
+              role_type = result.user.role_type
+              user_id = result.user._id
+            }
+            const userInfo = {
+              role_type: role_type,
+              user_id: user_id,
+              wechat_phone: result.user.wechat_phone
+            }
+            wx.setStorageSync('userInfo', userInfo)
+            wx.request({
+              url: baseUrl + '/tracings/' + id,
+              success: res => {
+                wx.hideLoading()
+                const result = res.data.data.data
+                _this.data.isOnwer = result.owner._id == userInfo.user_id ? true : false
+                _this.setData({
+                  key: result._id
+                })
+                let records = result.records;
+                const recordsLength = records.length
+                if (recordsLength === 0 || ['BIND', 'UNBIND'].includes(result.state)) {
+                  wx.showToast({
+                    title: '未发货，无法查看溯源码详情',
+                    icon: 'none',
+                    duration: 3000
+                  })
+                  return
+                } else {
+                  _this.setData({
+                    showDetail: true
+                  })
+                  wx.showToast({
+                    title: '主人好，请鉴',
+                    icon: 'none',
+                    duration: 3000
+                  })
+                }
+                const latestRecord = records.slice(recordsLength - 1, recordsLength)[0]
+                let hasCommitRight = true
+                if (latestRecord.reciver_type === 'business') {
+                  hasCommitRight = latestRecord.reciver === userInfo.user_id
+                } else {
+                  // 若为消费者，则判断是否需要获取手机号
+                  if (latestRecord.reciver_phone) {
+                    // 指定了收货人电话
+                    if (userInfo.wechat_phone) {
+                      hasCommitRight = latestRecord.reciver_phone === userInfo.wechat_phone
+                    } else {
+                      _this.setData({
+                        showDialog: true
+                      })
+                    }
+                  } else {
+                    hasCommitRight = latestRecord.sender !== userInfo.user_id
+                  }
+                }
+                let steps_temp = []
+                let banner = ''
+                for (let i = 0; i < records.length; i++) {
+                  const sender = records[i].sender
+                  const name = sender[sender.role_type].name
+                  steps_temp[i] = {
+                    text: records[i].express_name || name,
+                    desc: records[i].express_no || util.convertUTCTimeToLocalTime(records[i].send_at),
+                  }
+                  if (i == records.length - 1) {
+                    banner = sender[sender.role_type].banner
+                    _this.setData({
+                      hasReseverInfo: records[i].reciver_name ? true : false
+                    })
+                  }
+                }
+                const owner = result.owner
+                const state = result.state
+                let notice_text = ""
+                if (latestRecord.sender._id === userInfo.user_id & state === 'SEND') {
+                  wx.showToast({
+                    title: '您已发货，只能查看溯源码详情',
+                    icon: 'none',
+                    duration: 3000
+                  })
+                }
+                if (state == "BIND") {
+                  notice_text = "正品待售"
+                } else if (state == "SEND") {
+                  if (latestRecord.reciver_type == "consumer") {
+                    notice_text = "正品在" + util.getHours(latestRecord.send_at) + "小时前被出售"
+                  } else {
+                    notice_text = "正品待售"
+                  }
+                } else if (state == "RECEIVED") {
+                  if (owner._id === userInfo.user_id) _this.setData({
+                    hasRight: true
+                  })
+                  if (res.data.data.data.isEnd == true) {
+                    notice_text = "正品在:" + util.getHours(latestRecord.reciver_at) + "小时前被签收"
+                  } else {
+                    notice_text = "正品待售"
+                  }
+                }
+                _this.setData({
+                  bind_goods: result.products,
+                  firstGoods: result.products[0],
+                  goodsTotal: result.products.length,
+                  steps: steps_temp,
+                  banner: banner,
+                  id: id,
+                  showCommit: ["SEND", "EXPRESSED"].includes(state) && hasCommitRight ? true : false,
+                  isReceved: state == "RECEIVED" ? true : false,
+                  notice_text: notice_text,
+                })
+              },
+              fail: () => {
+                wx.hideLoading()
+              }
+            })
+          }
+        })
+      }
+    })
   },
 
   /**
