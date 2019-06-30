9d4a9b199b99a7d68394af5733dfc34f78c26249
Jimmy_Han <seafall@foxmail.com>
2019-06-30 12:37:50 +0800

    feat: modify tracingManage

--- pages/bigdata/tracingManage/tracingManage.js
+++ pages/bigdata/tracingManage/tracingManage.js
@@ -7,62 +7,126 @@ Page({
    * 页面的初始数据
    */
   data: {
+    userInfo: {},
     baseUrl: baseUrl,
-    count: 0,
-    counterfeits: [],
+    // count: 0,
+    // counterfeits: [],
+    statistics: {},
+    tracingList: [],
+    paginationPars: {limit: 10, offset: 0, sortByState: false, sort: '-created_at'},
     results: ['误把新包装当做假货', '其它']
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-    const userInfo = wx.getStorageSync('userInfo');
-    // 获假货
+  onLoad: function (options) {
+    this.setData({userInfo: userInfo});
+    this.getStatisticsData();
+    this.getTracingsData();
+  },
+
+  /**
+   * 获取溯源码使用情况的接口
+   */
+  getStatisticsData: function () {
+    const {baseUrl, userInfo} = this,
+        {user_id} = userInfo;
     wx.request({
-      url: baseUrl + '/tracings?owner=' + userInfo.user_id,
+      url: `${baseUrl}/users/${user_id}/statistics`,
       success: res => {
+        console.log(res);
         const data = res.data.data;
-        this.setData({
-          count: data.meta.count,
-          counterfeits: data.data
-        });
+        this.setData({statistics: data});
       }
     });
   },
 
   /**
+   * 获取溯源码列表的接口
+   */
+  getTracingsData: function () {
+    const {baseUrl, userInfo, tracingList, paginationPars} = this,
+        {user_id} = userInfo,
+        {limit, offset, sortByState, sort} = paginationPars;
+    const format = data => {
+      return data.map(item => {
+        if (item.state === 'BIND') {
+          item.state_display = '已绑定';
+        } else if (item.state === 'RECEIVED') {
+          item.state_display = '已签收';
+        } else {
+          // SEND EXPRESSED
+          item.state_display = '已发货';
+        }
+        item.lastRecord = {};
+        if (item.records.length > 0) {
+          item.lastRecord = item.records.slice(-1);
+          let send_time = new Date(item.send_at),
+              year = send_time.getFullYear(),
+              month = send_time.getMonth() + 1,
+              day = send_time.getDate(),
+              hour = send_time.getHours(),
+              minute = send_time.getMinutes();
+          hour = hour > 9 ? hour : '0' + hour;
+          minute = minute > 9 ? minute : '0' + minute;
+          item.lastRecord.send_at_display = `${year}/${month}/${day} ${hour}:${minute}`;
+        }
+        return item;
+      });
+    }
+    wx.request({
+      url: `${baseUrl}/tracings?owner=${user_id}&limit=${limit}&offset=${offset}&sortByState=${sortByState}&sort=${sort}`,
+      success: res => {
+        console.log(res);
+        const data = format(res.data.data),
+            newTracingList = tracingList.push(data);
+        this.setData({
+          tracingList: newTracingList
+        });
+      }
+    });
+  },
+  /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {},
+  onReady: function () {
+  },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {},
+  onShow: function () {
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {},
+  onHide: function () {
+  },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {},
+  onUnload: function () {
+  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {},
+  onPullDownRefresh: function () {
+  },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {},
+  onReachBottom: function () {
+    this.getTracingsData();
+  },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {}
+  onShareAppMessage: function () {
+  }
 });
